f350e1821dd230fb8fc02b87ebaf82381babadfb
João Aires <48353968+joaoaires@users.noreply.github.com>
2020-05-08 14:46:22 +0100

    atualizaçoes no menu inicial

--- app/src/main/java/pt/ipg/coronasports/MainActivity.java
+++ app/src/main/java/pt/ipg/coronasports/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipg.coronasports;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void bpaises(View view) {
+        Intent intent = new Intent(this, activity_pais.class);
+        startActivity(intent);
     }
 
     public void bequipas(View view) {
---
+++ app/src/main/java/pt/ipg/coronasports/activity_equipa.java
@@ -0,0 +1,6 @@
+package pt.ipg.coronasports;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_equipa extends AppCompatActivity {
+}
